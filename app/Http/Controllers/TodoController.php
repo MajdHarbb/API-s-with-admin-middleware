d7e8e4a8b85b99008c2370333e6b324f02917982
Majd Harb <97592562+MajdHarbb@users.noreply.github.com>
2023-04-15 11:19:52 +0300

    error handling in todos controller

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Todo;
+use App\Models\User;
 
 class TodoController extends Controller
 {
@@ -22,71 +23,110 @@ class TodoController extends Controller
         // ]);
         
         //with relationship
-        $user = User::find($request->user_id);
-        $todos = $user->todos;
-        return response()->json([
-            'status' => 'success',
-            'todos' => $todos,
-        ]);
+        try {
+            $user = User::find($request->user_id);
+            $todos = $user->todos;
+            return response()->json([
+                'status' => 'success',
+                'todos' => $todos,
+            ]);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                'message' => $th->getMessage()
+            ], 500);
+        }
+        
     }
 
     public function addTodo(Request $request)
     {
-        $request->validate([
-            'title' => 'required|string|max:255',
-            'description' => 'required|string|max:255',
-        ]);
-
-        $todo = Todo::create([
-            'user_id' => $request->user_id,
-            'title' => $request->title,
-            'description' => $request->description,
-        ]);
-
-        return response()->json([
-            'status' => 'success',
-            'message' => 'Todo created successfully',
-            'todo' => $todo,
-        ]);
+        try {
+            $request->validate([
+                'title' => 'required|string|max:255',
+                'description' => 'required|string|max:255',
+            ]);
+    
+            $todo = Todo::create([
+                'user_id' => $request->user_id,
+                'title' => $request->title,
+                'description' => $request->description,
+            ]);
+    
+            return response()->json([
+                'status' => 'success',
+                'message' => 'Todo created successfully',
+                'todo' => $todo,
+            ]);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                'message' => $th->getMessage()
+            ], 500);
+        }
+        
     }
 
-    public function getTodo($id)
+    public function getTodo(Request $request)
     {
-        $todo = Todo::find($id);
-        return response()->json([
-            'status' => 'success',
-            'todo' => $todo,
-        ]);
+        try {
+            $todo = Todo::find($request->user_id);
+            return response()->json([
+                'status' => 'success',
+                'todo' => $todo,
+            ]);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                'message' => $th->getMessage()
+            ], 500);
+        }
+        
     }
 
-    public function updateTodo(Request $request, $id)
+    public function updateTodo(Request $request)
     {
-        $request->validate([
-            'title' => 'required|string|max:255',
-            'description' => 'required|string|max:255',
-        ]);
-
-        $todo = Todo::find($id);
-        $todo->title = $request->title;
-        $todo->description = $request->description;
-        $todo->save();
-
-        return response()->json([
-            'status' => 'success',
-            'message' => 'Todo updated successfully',
-            'todo' => $todo,
-        ]);
+        try {
+            $request->validate([
+                'title' => 'required|string|max:255',
+                'description' => 'required|string|max:255',
+            ]);
+    
+            $todo = Todo::find($request->todo_id);
+            $todo->title = $request->title;
+            $todo->description = $request->description;
+            $todo->save();
+    
+            return response()->json([
+                'status' => 'success',
+                'message' => 'Todo updated successfully',
+                'todo' => $todo,
+            ]);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                'message' => $th->getMessage()
+            ], 500);
+        }
     }
 
     public function deleteTodo($id)
     {
-        $todo = Todo::find($id);
-        $todo->delete();
+        try {
+            $todo = Todo::find($id);
+            $todo->delete();
 
-        return response()->json([
-            'status' => 'success',
-            'message' => 'Todo deleted successfully',
-            'todo' => $todo,
-        ]);
+            return response()->json([
+                'status' => 'success',
+                'message' => 'Todo deleted successfully',
+                'todo' => $todo,
+            ]);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                'message' => $th->getMessage()
+            ], 500);
+        }
+        
     }
 }

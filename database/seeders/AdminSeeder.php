89d768c97b997790ac8e25da098b168ac4624cd7
Majd Harb <97592562+MajdHarbb@users.noreply.github.com>
2023-04-21 04:06:13 +0300

    admin seeder

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Seeder;
+use DB;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'first_name' => 'admin',
+            'last_name' => 'admin',
+            'email' => 'admin@admin.com',
+            'role' => 'admin',
+            'password' => Hash::make('password'),
+        ]);
+    }
+}
